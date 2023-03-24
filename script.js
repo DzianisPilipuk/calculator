b209c8bbc289a7bdedd73208ba69d67b7c56910f
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-24 18:52:20 +0100

    Add basic HTML structure

---
+++ script.js
@@ -0,0 +1,7 @@
+let displayPrevious = "";
+let displayCurrent = "0";
+const previousOperation = document.getElementById("previous-operation");
+const currentOperation = document.getElementById("current-operation");
+
+previousOperation.textContent = displayPrevious;
+currentOperation.textContent = displayCurrent;
