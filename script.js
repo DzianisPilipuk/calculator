3756ec0e6fa4bb0d48578a3caa84e67001e68a06
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-25 14:51:13 +0100

    Rebuild script structure

--- script.js
+++ script.js
@@ -1,65 +1,92 @@
-let displayPrevious = "";
-let operand_1;
-let operand_2;
+let previousOperand;
+let currentOperand = "";
 let operator;
-let displayCurrent = "0";
-const previousOperation = document.getElementById("previous-operation");
-const currentOperation = document.getElementById("current-operation");
+const previousData = document.getElementById("previous-data");
+const currentData = document.getElementById("current-data");
 let operatorAvailable = false;
 const operators = ["/", "*", "-", "+"];
+const equationSymbols = ["Enter", "Equal", "NumpadEnter"]
+let unlock = true;
 
-currentOperation.textContent = displayCurrent;
+document.addEventListener("keydown", addChar);
 
-document.addEventListener("keydown", addDigit);
+// get the number
+function addChar(e) {
+    if (!isNaN(e.key)) {addDigit(e.key)};
+    if (equationSymbols.includes(e.code) && currentOperand) {equation()};
+    if (operators.includes(e.key)) {addOperator(e.key)};
+}
 function addDigit(e) {
-    if (!isNaN(e.key)) {
-        if (displayCurrent == 0) {
-            displayCurrent = "";
-            updateCurrent();
-        }
-        displayCurrent += e.key;
-        updateCurrent();
-        operatorAvailable = true;
+    if (operator && unlock) {
+        previousOperand = currentOperand;
+        previousData.textContent = previousOperand + operator;
+        currentOperand = "";
+        unlock = false;
     }
-    if (operators.includes(e.key) && operatorAvailable) {
-        displayPrevious = displayCurrent;
-        if (operator) {
-            operand_2 = displayCurrent;
-        }
-        else {
-            operand_1 = displayCurrent;
-        }
-        displayCurrent = 0;
-        if (operator) {
-            displayPrevious = evaluate();
-            updatePrevious();
-            operand_1 = displayPrevious;
-        }
-        displayPrevious += e.key;
-        updatePrevious();
-        operator = e.key;
-        currentOperation.textContent = displayCurrent + ".";
-        displayCurrent = "0";
-        operatorAvailable = false;
+    currentOperand += e;
+    currentData.textContent = currentOperand;
+}
+function equation() {
+    previousData.textContent = previousOperand + operator + currentOperand + "="
+    previousOperand = evaluate();
+    currentData.textContent = previousOperand;
+    unlock = true;
+}
+function addOperator(e) {
+    operator = e;
+    if (currentOperand) {
+        previousData.textContent = currentOperand + e;
     }
+    else {
+        previousData.textContent = previousOperand + e;
+    }
+    currentData.textContent = "0";
+    unlock = true;
 }
 function evaluate() {
     if (operator == "+") {
-        return ~~operand_1 + ~~operand_2
+        return ~~previousOperand + ~~currentOperand
     }
     if (operator == "-") {
-        return ~~operand_1 - ~~operand_2
+        return ~~previousOperand - ~~currentOperand
     }
     if (operator == "*") {
-        return ~~operand_1 * ~~operand_2
+        return ~~previousOperand * ~~currentOperand
     }
     if (operator == "/") {
-        return ~~operand_1 / ~~operand_2
+        return ~~previousOperand / ~~currentOperand
     }
 }
-function updateCurrent() {
-    currentOperation.textContent = displayCurrent;
-}
-function updatePrevious() {
-    previousOperation.textContent = displayPrevious;
-}
+// proceed operation
+
+// function addChar(e) {
+//     if (!isNaN(e.key)) {
+//         if (displayCurrent == 0) {
+//             displayCurrent = "";
+//             updateCurrent();
+//         }
+//         displayCurrent += e.key;
+//         updateCurrent();
+//         operatorAvailable = true;
+//     }
+//     if (operators.includes(e.key) && operatorAvailable) {
+//         displayPrevious = displayCurrent;
+//         if (operator) {
+//             operand_2 = displayCurrent;
+//         }
+//         else {
+//             operand_1 = displayCurrent;
+//         }
+//         displayCurrent = 0;
+//         if (operator) {
+//             displayPrevious = evaluate();
+//             updatePrevious();
+//             operand_1 = displayPrevious;
+//         }
+//         displayPrevious += e.key;
+//         updatePrevious();
+//         operator = e.key;
+//         currentOperation.textContent = displayCurrent + ".";
+//         displayCurrent = "0";
+//     }
+// }
